c8207b6f828814fb01c222d25bc2b70005c2422d
Rameez_Iqbal <Rameez_Iqbal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 17:16:09 +0000

    New version
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-panels/trunk@954284 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Bootstrap Panels
  * Plugin URI: http://webcodingplace.com/bootstrap-panels/
  * Description: A simple way to create bootstrap panels in non-bootstrap sites .
- * Version: 1.6.1
+ * Version: 1.6.2
  * Author: Rameez
  * Author URI: http://webcodingplace.com/
  * License: GPLv2 or later
@@ -24,38 +24,38 @@ class nm_bspanels extends WP_Widget {
 	public function form($instance) {
 		extract($instance);
 	
-?>
-<p>
-	<label for="<?php echo $this->get_field_id('title'); ?>">Panel Heading: </label>
-	<input class="widefat"
-		id="<?php echo $this->get_field_id('title'); ?>"
-		name="<?php echo $this->get_field_name('title'); ?>"
-		value="<?php if (isset($title)) echo esc_attr($title); ?>"
-	/>
-</p>
+	?>
+	
+	<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>">Panel Heading: </label>
+		<input class="widefat"
+			id="<?php echo $this->get_field_id('title'); ?>"
+			name="<?php echo $this->get_field_name('title'); ?>"
+			value="<?php if (isset($title)) echo esc_attr($title); ?>"
+		/>
+	</p>
 
-<p>
-	<label for="<?php echo $this->get_field_id('content'); ?>">Panel Content: </label>
-	<textarea class="widefat"
-		rows="5"
-		id="<?php echo $this->get_field_id('content'); ?>"
-		name="<?php echo $this->get_field_name('content'); ?>"><?php if (isset($content)) echo esc_attr($content); 
-	?></textarea>
-</p>
+	<p>
+		<label for="<?php echo $this->get_field_id('content'); ?>">Panel Content: </label>
+		<textarea class="widefat"
+			rows="5"
+			id="<?php echo $this->get_field_id('content'); ?>"
+			name="<?php echo $this->get_field_name('content'); ?>"><?php if (isset($content)) echo esc_attr($content); 
+		?></textarea>
+	</p>
 
-<p>
-	<label for="<?php echo $this->get_field_id('color'); ?>">Panel Type: </label>
-	<input class="widefat"
-		id="<?php echo $this->get_field_id('color'); ?>"
-		name="<?php echo $this->get_field_name('color'); ?>"
-		value="<?php if (isset($color)) echo esc_attr($color); else { echo 'default';} ?>"
-	/>
-</p>
-
-<p>
-	Hint: primary, success, info, warning, danger
-</p>
-<?php
+	<p>
+		<label for="<?php echo $this->get_field_id('color'); ?>">Panel Color: </label>
+		<select class="widefat" id="<?php echo $this->get_field_id('color'); ?>" name="<?php echo $this->get_field_name('color'); ?>">
+			<option value="default"<?php if($color == 'default'){echo 'selected';} ?>>Default</option>
+			<option value="primary" <?php if($color == 'primary'){echo 'selected';} ?>>Blue</option>
+			<option value="success" <?php if($color == 'success'){echo 'selected';} ?>>Light Green</option>
+			<option value="warning"<?php if($color == 'warning'){echo 'selected';} ?>>Light Gold</option>
+			<option value="info"<?php if($color == 'info'){echo 'selected';} ?>>Light Blue</option>
+			<option value="danger"<?php if($color == 'danger'){echo 'selected';} ?>>Light Pink</option>
+		</select>
+	</p>
+	<?php
 
 	}
 
