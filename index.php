c3b44df6f183b680abb70d4e7235ae390be0c67b
Rameez_Iqbal <Rameez_Iqbal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 16:20:03 +0000

    A new version
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-panels/tags/1.6.1@954256 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Plugin Name: Bootstrap Panels
+ * Plugin URI: http://webcodingplace.com/bootstrap-panels/
+ * Description: A simple way to create bootstrap panels in non-bootstrap sites .
+ * Version: 1.6.1
+ * Author: Rameez
+ * Author URI: http://webcodingplace.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+class nm_bspanels extends WP_Widget {
+
+	function __construct() {
+		$param = array(
+			'description' 	=> 	'Creates bootstrap panels',
+			'name'			=>	'Bootstrap Panels'
+		);
+
+		parent::__construct('nm_panels','',$param);
+	}
+
+	public function form($instance) {
+		extract($instance);
+	
+?>
+<p>
+	<label for="<?php echo $this->get_field_id('title'); ?>">Panel Heading: </label>
+	<input class="widefat"
+		id="<?php echo $this->get_field_id('title'); ?>"
+		name="<?php echo $this->get_field_name('title'); ?>"
+		value="<?php if (isset($title)) echo esc_attr($title); ?>"
+	/>
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id('content'); ?>">Panel Content: </label>
+	<textarea class="widefat"
+		rows="5"
+		id="<?php echo $this->get_field_id('content'); ?>"
+		name="<?php echo $this->get_field_name('content'); ?>"><?php if (isset($content)) echo esc_attr($content); 
+	?></textarea>
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id('color'); ?>">Panel Type: </label>
+	<input class="widefat"
+		id="<?php echo $this->get_field_id('color'); ?>"
+		name="<?php echo $this->get_field_name('color'); ?>"
+		value="<?php if (isset($color)) echo esc_attr($color); else { echo 'default';} ?>"
+	/>
+</p>
+
+<p>
+	Hint: primary, success, info, warning, danger
+</p>
+<?php
+
+	}
+
+	public function widget($args, $instance) {
+	
+		extract($args);
+		extract($instance);
+	?>
+	<style>
+	<?php 
+		include_once('panels-style.php');
+	?>
+	</style>
+		<div class="panel panel-<?php echo $color; ?>">
+		  <div class="panel-heading">
+		    <h3 class="panel-title"><?php echo $title; ?></h3>
+		  </div>
+		  <div class="panel-body">
+		    <?php echo $content; ?>
+		  </div>
+		</div>
+
+	<?php
+	}
+	
+}
+
+add_action ('widgets_init','nm_register_bspanels');
+
+function nm_register_bspanels(){
+	register_widget('nm_bspanels');
+}
+ ?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Bootstrap Panels ===
+Contributors: Rameez_Iqbal
+Donate link: http://webcodingplace.com/contact-us/
+Tags: bootstrap, panels, widget, theme
+Requires at least: 3.3
+Tested up to: 3.9.1
+Stable tag: 1.6.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This simple plugin adds bootstrap panels widget to non bootstrap sites also.
+
+== Description ==
+
+This simple plugin adds bootstrap panels widget to non bootstrap sites also. No extra load on site, this is simplest plugin that will not effect your site's performance.
+
+
+== Installation ==
+
+1. Go to plugins in your dashboard and select 'add new'
+2. Search for 'Bootstrap Panels' and install it
+3. Go to Appearance > Widgets, and drag the bootstrap panels widget to your desired widget
+4. Enter heading and content and choose style for panel, click save
+5. Now visit your site and see awesome panels
+
+== Frequently Asked Questions ==
+
+= Can I use multiple panels with different styles? =
+
+Yes of course, just drag and drop where you want to show the panels.
+
+
+== Screenshots ==
+
+1. Just drag and drop where you want to create panels.
